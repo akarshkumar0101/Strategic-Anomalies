3eeb213d91e024aa2021e5a0a52f3969f79c9587
quinnchildress <quinnchildress@gmail.com>
2017-08-30 18:23:12 -0500

    Added a sound class/file to the game. It works, but have to insert the specific sound in the game.

---
+++ src/game/sound/Sound.java
@@ -0,0 +1,32 @@
+package game.sound;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Sound { {
+	/*insert sound into the game... Quinn*/
+    try {
+        // Open an audio input stream.
+        java.net.URL url = this.getClass().getClassLoader().getResource("resources/sound/sound15.wav");
+        AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+        // Get a sound clip resource.
+        Clip clip = AudioSystem.getClip();
+        // Open audio clip and load samples from the audio input stream.
+        clip.open(audioIn);
+        clip.start();
+     } catch (UnsupportedAudioFileException e) {
+        e.printStackTrace();
+     } catch (IOException e) {
+        e.printStackTrace();
+     } catch (LineUnavailableException e) {
+        e.printStackTrace();
+     }
+  
+}
+
+}
